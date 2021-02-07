307f82b4a82fb2189956a5d3f452a11a6ddba318
kimyerimweb <kimyerimweb@gmail.com>
2021-02-07 12:12:28 +0900

    시계 생성

---
+++ README.md
@@ -0,0 +1,20 @@
+# TodoList
+
+프로젝트 진행 상황에 따라 내용이 변경될 수 있습니다.
+
+## 소개
+
+- JavaScript 실습을 위한 To-do List 작성
+- 기간: 21.02.07 - 21.
+- 제작자: kimyerimweb@gmail.com
+
+## 제공 기능
+
+- 시계 (24시간 표현, 시,분까지 지원)
+
+## 기술
+
+- HTML5
+- CSS
+- JavaScript
+- Git,Github
---
+++ assets/js/clock.js
@@ -0,0 +1,16 @@
+const time = document.querySelector(".js-clock");
+
+function getTime(){
+    const date = new Date;
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    time.innerText = `${hours < 10 ? `0${hours}`: hours}:${
+                        minutes < 10 ? `0${minutes}`: minutes}`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime,1000);
+}
+
+init()
